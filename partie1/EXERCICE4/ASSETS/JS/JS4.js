b3dc8ed6d73a19a542bb1016e6a4ca05d7975667
Hugo31 <hugo.caron@live.fr>
2020-11-03 12:27:54 +0100

    avec correction du 4

--- partie1/EXERCICE4/ASSETS/JS/JS4.js
+++ partie1/EXERCICE4/ASSETS/JS/JS4.js
@@ -1,10 +1,9 @@
-
- document.getElementById('clickbutton').onclick =showAlert;
- function showAlert(){
-     var nom = document.getElementById('nom').value ;
-     var prenom = document.getElementById('prenom').value;
-     var ville = document.getElementById('ville')
-     alert(' bonjour'+' ' + nom +' '+prenom );
+document.getElementById('clickButton').onclick = showAlert; // apl de la fonction showalert au clic de clickbutton
+function showAlert(){ // declaration de la fonction showalert
+     var lastname = document.getElementById('lastname').value; // récupération du nom
+     var firstname = document.getElementById('firstname').value; //récupération du prénom
+     var city = document.getElementById('city').value;//récupération de la ville
+     alert('nom : ' + lastname + '\nprenom : '+ firstname+ '\nville:'+ city);  // affichage de la boite de dialogue
  }
  
 
