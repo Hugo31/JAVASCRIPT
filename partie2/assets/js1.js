507dda0b7fc12ef6158ea191809c3c73ac7f0ae1
Hugo31 <hugo.caron@live.fr>
2020-11-04 12:18:55 +0100

    partie 2

---
+++ partie2/assets/js1.js
@@ -0,0 +1,8 @@
+function newimg1()
+{
+    document.getElementById('pict1').src = '../partie2/assets/img/2575063-poutine44.jpg';
+}
+function newimg2()
+{
+    document.getElementById('pict1').src = '../partie2/assets/img/w_i.jpeg';
+}
